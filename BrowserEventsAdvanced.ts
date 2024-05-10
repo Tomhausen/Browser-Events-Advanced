1a19a323ca863f7c8c18e704831be47743cec172
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2024-05-10 17:51:19 +0100

    Initial files for MakeCode project

---
+++ BrowserEventsAdvanced.ts
@@ -0,0 +1,19 @@
+namespace browserEvents{
+
+    //% blockId=getMouseX
+    //% block="getMouseX"
+    //% group="Mouse"
+    //% weight=2
+    export function getMouseX(): number{
+        return mouseX();
+    }
+
+    //% blockId=getMouseY
+    //% block="getMouseY"
+    //% group="Mouse"
+    //% weight=1
+    export function getMousey(): number {
+        return mouseY();
+    }
+
+}
