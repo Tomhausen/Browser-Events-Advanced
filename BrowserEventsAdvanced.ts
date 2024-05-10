28d7eb0482eccca1472a61b575a3acc7b0c3e8d5
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2024-05-10 18:49:31 +0100

    added access to mouse on scene level

--- BrowserEventsAdvanced.ts
+++ BrowserEventsAdvanced.ts
@@ -1,19 +1,35 @@
 namespace browserEvents{
 
-    //% blockId=getMouseX
-    //% block="getMouseX"
+    //% blockId=getMouseCameraX
+    //% block="getMouseCameraX"
     //% group="Mouse"
-    //% weight=2
-    export function getMouseX(): number{
+    //% weight=4
+    export function getMouseCameraX(): number{
         return mouseX();
     }
 
-    //% blockId=getMouseY
-    //% block="getMouseY"
+    //% blockId=getMouseCameraY
+    //% block="getMouseCameraY"
+    //% group="Mouse"
+    //% weight=3
+    export function getMouseCameraY(): number {
+        return mouseY();
+    }
+
+    //% blockId=getMouseSceneX
+    //% block="getMouseSceneX"
+    //% group="Mouse"
+    //% weight=2
+    export function getMouseSceneX(): number{
+        return getMouseCameraX() + scene.cameraProperty(CameraProperty.Left)
+    }
+
+    //% blockId=getMouseSceneY
+    //% block="getMouseSceneY"
     //% group="Mouse"
     //% weight=1
-    export function getMouseY(): number {
-        return mouseY();
+    export function getMouseSceneY(): number {
+        return getMouseCameraY() + scene.cameraProperty(CameraProperty.Top)
     }
 
 }
